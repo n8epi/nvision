a97d545470e10c9e42010367d72a03fa1c40a049
n8epi <nate.strawn@gmail.com>
2016-05-16 16:49:42 -0400

    Update basics.

--- nvision/__init__.py
+++ nvision/__init__.py
@@ -1,8 +1,9 @@
 import numpy
 import matplotlib as plt
+import tensorflow as tf
 import sys
 
-sys.path.insert(0,'/Users/ns935/PycharmProjects/nvision/bh_tsne')
+sys.path.insert(0,'../bh_tsne')
 
 import bhtsne
 
